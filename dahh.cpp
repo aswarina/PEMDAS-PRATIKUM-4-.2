5a6e8b930059625cc4ea9901265495742b1639e6
aswarina <lyxsyyawa@gmail.com>
2026-04-01 12:15:54 +0700

    perubahan return true

--- dahh.cpp
+++ dahh.cpp
@@ -21,8 +21,12 @@ bool isFibonacci(int n) {
     int a = 0, b = 1;
     if (n == a || n == b) return true;
     int c = a + b;
-    while (c <= n) return true;
-    a = b;
-    b = c;
-    c = a + b;
+    while (c <= n) {
+        if (c == n) return true;
+        a = b;
+        b = c;
+        c = a + b;
+
+    }
+     
 }
